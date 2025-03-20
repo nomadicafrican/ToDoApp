3cda8ab3d3a0a5558c7476ef6e3eff54a9eb8265
Ammar <ammar.kusow2@gmail.com>
2025-03-20 18:06:11 -0400

    Initial commit: Added base structure for To-Do App

---
+++ TaskDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskDetailView.swift
+//  ToDoApp
+//
+//  Created by Ammar Ali on 2025-03-20.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskDetailView()
+    }
+}
---
+++ ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Ammar Ali on 2025-03-20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
